1a2768af4af8652709a1896c6aa8c0b182e4dce9
Sebastian Hillig <sebastian.hillig@gmail.com>
2010-08-15 15:32:08 +0200

    Unittest nice

--- journeyman/builds/views.py
+++ journeyman/builds/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
 
-from journeyman.builds.models import Build, BuildState
+from journeyman.builds.models import Build, BuildResult, BuildState
 from journeyman.builds.tasks import BuildTask
 
 from journeyman.workers.models import BuildNode
 from journeyman.projects.models import Project
+
+from lxml import etree
+from StringIO import StringIO
 
 def create(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
@@ -26,6 +29,55 @@ def detail(request, build_id):
 
     return render_to_response('builds/detail.html', {
         'object': build
+    }, context_instance=RequestContext(request))
+
+class TestSuite(object):
+    def __init__(self, attrs):
+        self.testcases = []
+        self.__dict__.update(**attrs)
+
+    def __unicode__(self):
+        return str(self.__dict__)
+
+class Test(object):
+    def __init__(self, attrs):
+        self.messages = []
+        self.error = False
+        self.failure = False
+        self.__dict__.update(**attrs)
+
+class Message(object):
+    def __init__(self, content, attrs):
+        self.content = content
+        self.__dict__.update(**attrs)
+    
+    def __unicode__(self):
+        return str(self.__dict__)
+        
+def detail_tests(request, build_id):
+    build = get_object_or_404(Build, pk=build_id)
+    suites = []
+    results = BuildResult.objects.filter(build=build)
+    for result in results:
+        s = StringIO(str(result.body))
+        root = etree.parse(s)
+        print etree.tostring(root, pretty_print=True)
+        for item in root.findall('.'):
+            ts = TestSuite(item.attrib)
+            suites.append(ts)
+            for test in item.iterchildren():
+                t = Test(test.attrib)
+                ts.testcases.append(t)
+                for message in test.iterchildren():
+                    if message.tag == "error":
+                        t.error = True
+                    elif message.tag == "failure":
+                        t.failure = True
+                    m = Message(message.text, message.attrib)
+                    t.messages.append(m)
+
+    return render_to_response('builds/detail_tests.html', {
+        'suites': suites
     }, context_instance=RequestContext(request))
 
 def delete(request, build_id):
---
+++ spielwiese/tests/big_test.py
@@ -0,0 +1,17 @@
+from calculon import sub, add, op
+
+from unittest import TestCase
+
+class LongTest(TestCase):
+    
+    def test_apocalypse(self):
+        assert(False != True, "God forbid")
+    
+    def test_dawn(self):
+        assert("something" != "something else", "Never!")
+        
+    def test_failfailfail(self)
+        op*op
+        
+    def test_dumn(self):
+        assert False, "Yeah, too easy?"
