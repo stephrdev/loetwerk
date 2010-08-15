49ee4ab4952251a7239fc0e712efc6c2cc2a7f60
Stephan Jaekel <steph@rdev.info>
2010-08-15 15:33:15 +0200

    added redirect

--- journeyman/urls.py
+++ journeyman/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
+from django.views.generic.simple import redirect_to
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^$', redirect_to, {'url': '/projects/'}),
     (r'^admin/', include(admin.site.urls)),
 
     (r'^projects/', include("journeyman.projects.urls")),
